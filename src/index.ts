2db2f402a2827c9fec5811a259e427f1235e368b
AricRedemption <aricredemption@gmail.com>
2024-05-11 19:03:19 +0800

    📦 VERSION: 0.3.3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import { genUID, formatIndex } from "./wallet-manager/tools";
+import {
+  CoinType,
+  SignType,
+  BtcWallet,
+  MvcWallet,
+  BaseWallet,
+  AddressType,
+} from "@metalet/utxo-wallet-sdk";
 
 export { genUID, formatIndex };
 export * from "./wallet-manager";
+export { BtcWallet, MvcWallet, BaseWallet, AddressType, CoinType, SignType };
--- src/wallet-manager/index.ts
+++ src/wallet-manager/index.ts
@@ -1,11 +1,2 @@
-import {
-  CoinType,
-  BtcWallet,
-  MvcWallet,
-  BaseWallet,
-  AddressType,
-} from "@metalet/utxo-wallet-sdk";
-
 export * from "./types";
 export * from "./manager";
-export { BtcWallet, MvcWallet, BaseWallet, AddressType, CoinType };
